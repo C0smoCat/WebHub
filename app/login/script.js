d7a7e3afb9afc6cb05deb40826a0dad9e9452e19
SkripMen <8/1>
2020-03-07 10:19:42 +0400

    6

--- app/login/script.js
+++ app/login/script.js
@@ -1,4 +1,4 @@
-function openCity(cityName, elmnt) {
+function openCity(cityName, elmnt, butname) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -6,11 +6,15 @@ function openCity(cityName, elmnt) {
     }
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.backgroundColor = "#8d8d8d";
+        tablinks[i].style.boxShadow = 'none';
     }
     document.getElementById(cityName).style.display = "grid";
+    document.getElementById(butname).style.backgroundColor = "#353534";
+    document.getElementById(elmnt).style.boxShadow = "rgba(0, 0, 0, 0.333) 0px -15px 10px -5px inset";
+
 
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+document.getElementById("auto_but").click();
