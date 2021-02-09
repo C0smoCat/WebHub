4d51cdc67068d586fafe60594193de639b8213ab
SkripMen <48603232+SkripMen@users.noreply.github.com>
2021-02-09 15:41:28 +0400

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@
 module.exports = {
     databaseConfig: {
         connectionLimit: 5,
-        host: "localhost",
-        user: "root",
-        database: "webhub",
-        password: "1234",
+        host: "n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+        user: "hdb6p9d3z3obyszl",
+        database: "ykik5i6tm9tdzbb1",
+        password: "vdpmm0t5f9g4f9ao",
         port: 3306,
         charset: "UTF8_GENERAL_CI",
         debug: false
