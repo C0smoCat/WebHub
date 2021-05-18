17c8aaae2d74f348ac9ebb5e1d4062c438b2f267
C0smoCat <50526934+C0smoCat@users.noreply.github.com>
2021-05-19 03:13:01 +0400

    config fix

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 "use strict";
 
 module.exports = {
-    port: 8080,
-    debug_mode: process.env.NODE_ENV !== 'production' || typeof v8debug === 'object' || /--debug|--inspect/.test(process.execArgv.join(' ') || process.env.NODE_OPTIONS.includes("debug"))
+    port: process.env.PORT || 8080,
 };
